1daa36d02669157b8ee3aed10c78730559ed4e23
AndresSanchez <naquerf@gmail.com>
2019-03-20 20:19:30 -0500

    segundo commit, se agrega a pom, dependencia para usar eclipse como "proveedor de persistencia"

--- target/generated-sources/annotations/com/tambo/model/VO/Question_.java
+++ target/generated-sources/annotations/com/tambo/model/VO/Question_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-19T22:47:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-20T20:18:53")
 @StaticMetamodel(Question.class)
 public class Question_ { 
 
