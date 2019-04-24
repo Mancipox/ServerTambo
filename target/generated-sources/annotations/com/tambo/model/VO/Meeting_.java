c454f565fdef0cc7863cbf149fad1dd4b0e18985
Mancipox <smancipeparedes@gmail.com>
2019-04-24 00:42:23 -0500

    Fixed database connection

--- target/generated-sources/annotations/com/tambo/model/VO/Meeting_.java
+++ target/generated-sources/annotations/com/tambo/model/VO/Meeting_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-23T23:57:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-24T00:36:28")
 @StaticMetamodel(Meeting.class)
 public class Meeting_ { 
 
