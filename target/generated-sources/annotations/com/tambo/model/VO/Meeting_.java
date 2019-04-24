a5b41333c1608b9371a7cd7221f3038e793ebc1a
AndresSanchez <naquerf@gmail.com>
2019-04-24 00:00:18 -0500

    cambio en glassfish resources 2

--- target/generated-sources/annotations/com/tambo/model/VO/Meeting_.java
+++ target/generated-sources/annotations/com/tambo/model/VO/Meeting_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-23T23:31:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-23T23:57:00")
 @StaticMetamodel(Meeting.class)
 public class Meeting_ { 
 
